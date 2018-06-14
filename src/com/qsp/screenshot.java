7df447e7a499232cf40ec981a049639ca066e6a5
Anupama CB <cbanupama03@gmail.com>
2018-06-14 17:48:35 +0530

    Screen Shot

--- src/com/qsp/screenshot.java
+++ src/com/qsp/screenshot.java
@@ -1,7 +1,9 @@
 package com.qsp;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class screenshot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -18,6 +20,10 @@ public class screenshot {
 		//to take screen shot
 		
 		File src = t.getScreenshotAs(OutputType.FILE);
+		System.out.println(src);
+		File dest = new File("./screenshot3610192782938907957.png");
+		FileUtils.copyFile(src, dest);
+		driver.close();
 
 	}
 
